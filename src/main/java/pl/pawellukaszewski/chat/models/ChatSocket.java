9fec0ed0f1cc04b52a0edbce64ae92f91e536706
PawelLukaszewski <pawel.t.lukaszewski@gmail.com>
2018-08-26 20:30:38 +0200

    Initial commit

--- src/main/java/pl/pawellukaszewski/chat/models/ChatSocket.java
+++ src/main/java/pl/pawellukaszewski/chat/models/ChatSocket.java
@@ -1,6 +1,8 @@
 package pl.pawellukaszewski.chat.models;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
@@ -8,6 +10,8 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 @EnableWebSocket
 public class ChatSocket extends TextWebSocketHandler {
 
-    
-
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        System.out.println("new message");
+    }
 }
