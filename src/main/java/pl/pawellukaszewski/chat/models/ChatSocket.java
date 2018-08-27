6522e453d1c6c49e8c5aa279d4a6b25f9dc12dc4
PawelLukaszewski <pawel.t.lukaszewski@gmail.com>
2018-08-27 18:54:01 +0200

    Initial commit

--- src/main/java/pl/pawellukaszewski/chat/models/ChatSocket.java
+++ src/main/java/pl/pawellukaszewski/chat/models/ChatSocket.java
@@ -4,14 +4,23 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 @Configuration
 @EnableWebSocket
-public class ChatSocket extends TextWebSocketHandler {
+public class ChatSocket extends TextWebSocketHandler implements WebSocketConfigurer {
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
+        webSocketHandlerRegistry.addHandler(this, "/chat").setAllowedOrigins("*");
+    }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         System.out.println("new message");
     }
+
+
 }
