a5eaf7d2f971c5434b08e115b7ae293cabe66389
PawelLukaszewski <pawel.t.lukaszewski@gmail.com>
2018-08-29 11:09:00 +0200

    Initial commit

--- src/main/java/pl/pawellukaszewski/chat/models/ChatSocket.java
+++ src/main/java/pl/pawellukaszewski/chat/models/ChatSocket.java
@@ -20,16 +20,16 @@ public class ChatSocket extends TextWebSocketHandler implements WebSocketConfigu
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        System.out.println("new message");
+        System.out.println("new message!");
     }
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        System.out.println("Somebody connect with socket");
+        System.out.println("Somebody connect with socket!");
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-        System.out.println("Somebody disconnected");
+        System.out.println("Somebody disconnected!");
     }
 }
